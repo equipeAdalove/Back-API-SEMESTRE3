823bdbc594f3ca5537fab4ac7650cd9e930b1e2b
Matheus Germano <silvaa.matheus112@gmail.com>
2025-03-29 20:11:53 -0300

    :wrench: chore: Ajuste na política CORS para permitir requisições gerais

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Ativar CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // Permitir apenas o frontend local
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
     allowedHeaders: 'Content-Type, Accept', // Cabeçalhos permitidos
   });
